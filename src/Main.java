f47ec6eec0874c6138dd5b0b750cb9176a0864bb
vladGogorita <vlgogorita@gmail.com>
2019-04-22 13:40:29 +0300

    Second class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        Test1 test1Object = new Test1();
+
         System.out.println("Hello World!");
         System.out.println("Hello Again!");
+        System.out.println(test1Object.getHelloMessage());
+        test1Object.sayHelloMessage();
     }
 }
--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,10 @@
 public class Test1 {
 
-    public static void main(String[] args) {
-        System.out.println("This is the second class, a Test class.");
+    public String getHelloMessage() {
+       return "This is the second class, a Test class.";
+    }
+
+    public void sayHelloMessage() {
+       System.out.println("This is the second class, a Test class.");
     }
 }
