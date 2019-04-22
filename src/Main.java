9accf4e24f8770b33a8e995e093073ba7f402e50
vladGogorita <vlgogorita@gmail.com>
2019-04-22 15:23:29 +0300

    v1.0.7

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Hello Again!");
         System.out.println(test1Object.getHelloMessage());
         test1Object.sayHelloMessage();
+
+
     }
 }
--- src/Test1.java
+++ src/Test1.java
@@ -7,4 +7,10 @@ public class Test1 {
     public void sayHelloMessage() {
        System.out.println("This is the second class, a Test class.");
     }
+
+    public static double average3(double a, double b, double c) {
+        double avg = (a + b + c)/3;
+
+        return avg;
+    }
 }
